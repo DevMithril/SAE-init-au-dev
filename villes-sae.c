fdd3cc8756d74acedc0b787c466724a44bcf4248
DevMithril <152490615+DevMithril@users.noreply.github.com>
2024-10-23 20:05:27 +0200

    Update villes-sae.c

--- villes-sae.c
+++ villes-sae.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 // define Types
@@ -17,7 +18,7 @@ typedef struct Tab_Cities
     City *cities;
 }Tab_Cities;
 
-// define Functions
+// define Functions & Procedures
 
 void load_from_csv(const char *file_path, Tab_Cities *tab)
 {
@@ -40,17 +41,33 @@ void save_csv(const char *file_path, Tab_Cities *tab)
     free(tab->cities);
 }
 
-void print_cities(Tab_Cities *cities)
+void add_city(Tab_Cities *tab)
+{
+    ;
+}
+
+void del_city(Tab_Cities *tab)
+{
+    ;
+}
+
+void chg_city(Tab_Cities *tab)
+{
+    ;
+}
+
+void print_cities(Tab_Cities *tab)
 {
     printf("CODE / VILLE / Latitude / Longitude\n");
-    for (int i = 0; i < cities->nb_cities; i++)
+    for (int i = 0; i < tab->nb_cities; i++)
     {
-        printf("%d / %s / %f / %f\n", cities->cities[i].code, cities->cities[i].name, cities->cities[i].latitude, cities->cities[i].longitude);
+        printf("%d / %s / %f / %f\n", tab->cities[i].code, tab->cities[i].name, tab->cities[i].latitude, tab->cities[i].longitude);
     }
 }
 
 void show_help(void)
 {
+    printf("--------------------------------------------------------------\n");
     printf("Commandes disponibles :\n");
     printf("h - affiche cette liste de commandes\n");
     printf("p - affiche les données de toutes les villes\n");
@@ -60,21 +77,12 @@ void show_help(void)
     printf("c - permet d'afficher les coordonnées GPS d'une ville\n");
     printf("w - enregistre les modifications et quitte le programme\n");
     printf("q - quitte le programme en abandonnant les modifications\n");
+    printf("--------------------------------------------------------------\n");
 }
 
-// define Variables
-
-Tab_Cities tab_cities = {.nb_cities = 0, .cities = NULL};
-char command;
-
-// main Function
-
-int main(void)
+bool execute_command(char cmd, bool *exit, Tab_Cities *tab_cities)
 {
-    load_from_csv("cities.csv", &tab_cities);
-    show_help();
-    scanf("%c", &command);
-    switch (command)
+    switch (cmd)
     {
         case 'h':
         {
@@ -83,19 +91,78 @@ int main(void)
         }
         case 'p':
         {
-            print_cities(&tab_cities);
+            print_cities(tab_cities);
             break;
         }
         case 'a':
         {
+            add_city(tab_cities);
+            break;
+        }
+        case 's':
+        {
+            del_city(tab_cities);
+            break;
+        }
+        case 'm':
+        {
+            chg_city(tab_cities);
+            break;
+        }
+        case 'c':
+        {
+            break;
+        }
+        case 'w':
+        {
+            save_csv("cities.csv", tab_cities);
+            *exit = true;
+            break;
+        }
+        case 'q':
+        {
+            free(tab_cities->cities);
+            *exit = true;
             break;
         }
         default:
         {
+            printf("Commande non reconnue : saisissez 'h' pour afficher la liste des commandes valides\n");
             break;
         }
     }
+}
 
-    save_csv("cities.csv", &tab_cities);
+// define Variables
+
+Tab_Cities tab_cities;
+char command;
+bool quit;
+
+// main Function
+
+int main(void)
+{
+    // initializing variables
+
+    tab_cities.nb_cities = 0;
+    tab_cities.cities = NULL;
+    quit = false;
+
+    // loading data & showing commands
+
+    load_from_csv("cities.csv", &tab_cities);
+    show_help();
+    
+    // main loop
+
+    while (!quit)
+    {
+        scanf("%c", &command);
+        execute_command(command, &quit, &tab_cities);
+    }
+
+    // quitting program
+
     return EXIT_SUCCESS;
 }
