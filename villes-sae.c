ad3777051c79c6015d55df2380fd459dde7e2bed
DevMithril <152490615+DevMithril@users.noreply.github.com>
2024-10-23 17:09:39 +0200

    Update villes-sae.c

--- villes-sae.c
+++ villes-sae.c
@@ -1,1 +1,23 @@
-//nice
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Ville
+{
+    int code;
+    float latitude, longitude;
+    char nom[80];
+}Ville;
+
+int main(void)
+{
+    Villes *liste_villes = malloc(sizeof(Ville));
+    FILE *csv = NULL;
+    csv = fopen("cities.csv", "r+");
+    if (csv != NULL)
+    {
+        liste_villes = realloc(liste_villes, sizeof(liste_villes)+sizeof(Ville));
+        fclose(csv);
+    }
+    free(liste_villes);
+    return 0;
+}
