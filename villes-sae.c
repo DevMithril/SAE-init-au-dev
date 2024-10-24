6ecfdb7aa8ca3fb56be3136143c9ca307513faf0
DevMithril <152490615+DevMithril@users.noreply.github.com>
2024-10-24 15:11:58 +0200

    Update villes-sae.c
    remplacement du tableau par une liste chainée

--- villes-sae.c
+++ villes-sae.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <math.h>
 
 // define Types
@@ -10,28 +11,48 @@ typedef struct City
     int code;
     float latitude, longitude;
     char name[85];
+    struct City *next;
 }City;
-
-typedef struct Tab_Cities
-{
-    int nb_cities;
-    City *cities;
-}Tab_Cities;
 
 // define Functions & Procedures
 
-void load_from_csv(const char *file_path, Tab_Cities *tab)
+void free_City(City *city, City *list)
+{
+    City *current = list;
+    City *tmp = NULL;
+    if (city == NULL)
+    {
+        printf("Error : invalid argument for 'free_City'\nProceed anyway\n");
+        return;
+    }
+    while (current->next != city)
+    {
+        current = current->next;
+    }
+    tmp = current->next->next;
+    free(current->next);
+    current->next = tmp;    
+}
+
+void free_Cities(City *list)
+{
+    while (list->next != NULL)
+    {
+        free_City(list->next, list);
+    }
+}
+
+void load_from_csv(const char *file_path, City *list)
 {
     FILE *csv = NULL;
-    City *new_city = NULL;
-    City tmp;
+    City *current = NULL;
     int nb_of_scan;
 
-    if (tab->cities != NULL)
+    if (list->next != NULL)
     {
-        free(tab->cities);
+        free_Cities(list);
     }
-    tab->nb_cities = 0;
+    current = list;
     
     csv = fopen(file_path, "r");
     if (csv == NULL)
@@ -43,47 +64,85 @@ void load_from_csv(const char *file_path, Tab_Cities *tab)
     fscanf(csv, "CODE,VILLE,LATITUDE,LONGITUDE");
     do
     {
-        tab->cities = realloc(tab->cities, sizeof(tab->cities) + sizeof(City));
-        tab->nb_cities += 1;
-        new_city = &tab->cities[tab->nb_cities - 1];
-        nb_of_scan = fscanf(csv, "%d,%[^,],%f,%f", &new_city->code, new_city->name, &new_city->latitude, &new_city->longitude);
+        current->next = malloc(sizeof(City));
+        if (current->next == NULL)
+        {
+            printf("Error : out of memory\n");
+            free_Cities(list);
+            fclose(csv);
+            exit(EXIT_FAILURE);
+        }
+        current = current->next;
+        nb_of_scan = fscanf(csv, "%d,%[^,],%f,%f", &current->code, current->name, &current->latitude, &current->longitude);
     } while (nb_of_scan != EOF);
-    tab->nb_cities -= 1;
+    free_City(current, list);
     fclose(csv);
 }
 
-void save_csv(const char *file_path, Tab_Cities *tab)
+void save_csv(const char *file_path, City *list)
 {
     FILE *csv = NULL;
-    free(tab->cities);
+    free_Cities(list);
 }
 
-void add_city(Tab_Cities *tab)
+void add_city(City *list)
 {
-    City *new_city = NULL;
-    printf("Veuillez renseigner les données de la ville à ajouter (format 'code nom latitude longitude')\n");
-    tab->cities = realloc(tab->cities, sizeof(tab->cities) + sizeof(City));
-    tab->nb_cities += 1;
-    new_city = &tab->cities[tab->nb_cities - 1];
-    scanf("%d %s %f %f", &new_city->code, new_city->name, &new_city->latitude, &new_city->longitude);
-}
-
-void del_city(Tab_Cities *tab)
-{
-    ;
-}
-
-void chg_city(Tab_Cities *tab)
-{
-    ;
-}
-
-void print_cities(Tab_Cities *tab)
-{
-    printf("CODE / VILLE / LATITUDE / LONGITUDE\n");
-    for (int i = 0; i < tab->nb_cities; i++)
+    City *last = list;
+    while (last->next != NULL)
     {
-        printf("%d / %s / %f / %f\n", tab->cities[i].code, tab->cities[i].name, tab->cities[i].latitude, tab->cities[i].longitude);
+        last = last->next;
+    }
+    printf("Veuillez renseigner les données de la ville à ajouter (format 'code nom latitude longitude')\n");
+    last->next = malloc(sizeof(City));
+    if (last->next == NULL)
+    {
+        printf("Error : out of memory\n");
+        free_Cities(list);
+        exit(EXIT_FAILURE);
+    }
+    scanf("%d %s %f %f", &last->next->code, last->next->name, &last->next->latitude, &last->next->longitude);
+}
+
+void del_city(City *list)
+{
+    char input[85];
+    City *current = list->next;
+    bool found = false;
+    printf("Saisissez le Nom de la ville à supprimer : \n");
+    scanf(" %[^\n]", input);
+    while (!found)
+    {
+        if (current == NULL)
+        {
+            printf("Nom de Ville invalide : saisissez 'p' pour afficher la liste des villes\n");
+            found = true;
+        }
+        else if (!(strcmp(input, current->name)))
+        {
+            free_City(current, list);
+            printf("La Ville %s a bien été supprimée.\n", input);
+            found = true;
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+}
+
+void chg_city(City *list)
+{
+    ;
+}
+
+void print_cities(City *list)
+{
+    City *current = list->next;
+    printf("CODE / VILLE / LATITUDE / LONGITUDE\n");
+    while (current != NULL)
+    {
+        printf("%d / %s / %f / %f\n", current->code, current->name, current->latitude, current->longitude);
+        current = current->next;
     }
 }
 
@@ -102,7 +161,7 @@ void show_help(void)
     printf("--------------------------------------------------------------\n");
 }
 
-bool execute_command(char cmd, bool *exit, Tab_Cities *tab_cities)
+bool execute_command(char cmd, bool *exit, City *list_cities)
 {
     switch (cmd)
     {
@@ -113,22 +172,22 @@ bool execute_command(char cmd, bool *exit, Tab_Cities *tab_cities)
         }
         case 'p':
         {
-            print_cities(tab_cities);
+            print_cities(list_cities);
             break;
         }
         case 'a':
         {
-            add_city(tab_cities);
+            add_city(list_cities);
             break;
         }
         case 's':
         {
-            del_city(tab_cities);
+            del_city(list_cities);
             break;
         }
         case 'm':
         {
-            chg_city(tab_cities);
+            chg_city(list_cities);
             break;
         }
         case 'c':
@@ -137,13 +196,13 @@ bool execute_command(char cmd, bool *exit, Tab_Cities *tab_cities)
         }
         case 'w':
         {
-            save_csv("cities.csv", tab_cities);
+            save_csv("cities.csv", list_cities);
             *exit = true;
             break;
         }
         case 'q':
         {
-            free(tab_cities->cities);
+            free_Cities(list_cities);
             *exit = true;
             break;
         }
@@ -157,7 +216,7 @@ bool execute_command(char cmd, bool *exit, Tab_Cities *tab_cities)
 
 // define Variables
 
-Tab_Cities tab_cities;
+City list_cities;
 char command;
 bool quit;
 
@@ -167,13 +226,12 @@ int main(void)
 {
     // initializing variables
 
-    tab_cities.nb_cities = 0;
-    tab_cities.cities = NULL;
+    list_cities.next = NULL;
     quit = false;
 
     // loading data & showing commands
 
-    load_from_csv("cities.csv", &tab_cities);
+    load_from_csv("cities.csv", &list_cities);
     show_help();
     
     // main loop
@@ -182,7 +240,7 @@ int main(void)
     {
         printf(">> ");
         scanf(" %c", &command);
-        execute_command(command, &quit, &tab_cities);
+        execute_command(command, &quit, &list_cities);
     }
 
     // quitting program
