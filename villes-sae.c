c358eecf4c0d02ca16435f1672adc19a0cbb843d
DevMithril <152490615+DevMithril@users.noreply.github.com>
2024-11-06 18:57:24 +0100

    Update villes-sae.c

--- villes-sae.c
+++ villes-sae.c
@@ -4,21 +4,28 @@
 #include <string.h>
 #include <math.h>
 
-#define PI 3.14159265359
+/* définitions des Constantes */
 
-/* define Types */
+#define PI 3.14159265359
+#define R_TERRE 6371
+#define MAX_LEN_CSV_FILENAME 50
+#define MAX_LEN_CODE 15
+#define MAX_LEN_NAME 30
+#define MAX_LEN_LAT_LONG 11
+
+/* définitions des Types */
 
 typedef struct City
 {
-    char code[15];
+    char code[MAX_LEN_CODE];
     float latitude, longitude;
-    char name[30];
+    char name[MAX_LEN_NAME];
     struct City *next;
 }City;
 
-/* define Functions & Procedures */
+/* définitions des Fonctions & Procédures */
 
-void free_City(City *city, City *list) // removes the city from the list and deallocates the memory reserved for the city
+void free_City(City *city, City *list) // retire la ville de la liste et déalloue la mémoire réservée pour la ville
 {
     City *current = list;
     City *tmp = NULL;
@@ -27,16 +34,16 @@ void free_City(City *city, City *list) // removes the city from the list and dea
         fprintf(stderr, "Error : invalid 'city' argument for 'free_City', proceed anyway\n");
         return;
     }
-    while (current->next != city) // finding the city preceding the one to delete
+    while (current->next != city) // obtention de al ville précédant celle à supprimer
     {
         current = current->next;
     }
-    tmp = current->next->next; // saving the end of the list in tmp
+    tmp = current->next->next; // sauvegarde de la fin de la liste dans tmp
     free(current->next);
-    current->next = tmp; // reattaching the end of the list    
+    current->next = tmp; // réattachement de la fin de la liste
 }
 
-void free_Cities(City *list) // removes all cities in the list
+void free_Cities(City *list) // supprime toutes les villes de la liste
 {
     while (list->next != NULL)
     {
@@ -44,15 +51,15 @@ void free_Cities(City *list) // removes all cities in the list
     }
 }
 
-void create_city(const char *code, const char *name, float lat, float lng, City *end_of_list, City *list) // allocates a city-sized chunk of memory and fills it with data
+void create_city(const char *code, const char *name, float lat, float lng, City *end_of_list, City *list) // alloue un emplacement mémoire de la taille d'une ville et le remplit avec les données
 {
     City *last = list;
-    while (last->next != NULL) // finding the last city of the list
+    while (last->next != NULL) // obtention de la dernière ville de la liste
     {
         last = last->next;
     }
     last->next = malloc(sizeof(City));
-    if (last->next == NULL) // if malloc fail, then we just quit the program after deallocating all memory
+    if (last->next == NULL) // si malloc échoue, alors on quitte le programme après avoir déalloué toute la mémoire
     {
         fprintf(stderr, "Error : out of memory\n");
         last->next = end_of_list;
@@ -64,30 +71,30 @@ void create_city(const char *code, const char *name, float lat, float lng, City 
     strcpy(last->name, name);
     last->latitude = lat;
     last->longitude = lng;
-    last->next = end_of_list; // attaching the possible end of list to the current list
+    last->next = end_of_list; // attachement de l'éventuelle fin de liste à la liste actuelle
 }
 
-void load_from_csv(const char *file_path, City *list) // loads cities data from a csv file into a list
+void load_from_csv(const char *file_path, City *list) // charge dans une liste des données de villes issues d'un fichier csv
 {
     FILE *csv = NULL;
     City *current = NULL;
     int nb_of_scan;
 
-    free_Cities(list); // if there is already any data, then delete it
+    free_Cities(list); // si il y a déjà des données, alors on les supprime
     current = list;
     
     csv = fopen(file_path, "r");
-    if (csv == NULL)
+    if (csv == NULL) // si l'ouverture du fichier échoue, alors on quite le programme
     {
         fprintf(stderr, "Error : can't open file '%s'\n", file_path);
         exit(EXIT_FAILURE);
     }
     
-    fscanf(csv, "CODE,VILLE,LATITUDE,LONGITUDE\n"); // reading the title line
+    fscanf(csv, "CODE,VILLE,LATITUDE,LONGITUDE\n"); // lecture de la ligne des titres
     do
     {
         current->next = malloc(sizeof(City));
-        if (current->next == NULL) // if malloc fail, then we just quit the program after deallocating all memory
+        if (current->next == NULL) // si malloc échoue, alors on quitte le programme après avoir déalloué toute la mémoire
         {
             fprintf(stderr, "Error : out of memory\n");
             free_Cities(list);
@@ -97,23 +104,23 @@ void load_from_csv(const char *file_path, City *list) // loads cities data from 
         current = current->next;
         nb_of_scan = fscanf(csv, "%[^,],%[^,],%f,%f\n", current->code, current->name, &current->latitude, &current->longitude);
     } while (nb_of_scan != EOF);
-    free_City(current, list); // freeing the last city as it wasn't filled with any data
+    free_City(current, list); // libération de la dernière ville car elle ne contient aucunes données
     fclose(csv);
 }
 
-City *search_City(const char *city_name, bool searching_previous, City *list) // returns a pointer to the city with the given city_name or the previous one if requested
+City *search_City(const char *city_name, bool searching_previous, City *list) // renvoie un pointeur vers la ville ayant le nom demandé ou celle qui précède si c'est demandé
 {
     City *current = list;
     City *return_value = NULL;
     bool found = false;
     while (!found)
     {
-        if (current->next == NULL) // if the searched city isn't is the given list
+        if (current->next == NULL) // si la ville recherchée n'est pas dans la liste donnée
         {
             printf("Nom de Ville invalide : saisissez 'p' pour afficher la liste des villes\n");
             found = true;
         }
-        else if (!(strcmp(city_name, current->next->name))) // if we found the searched city
+        else if (!(strcmp(city_name, current->next->name))) // si on a trouvé la ville recherchée
         {
             if (searching_previous)
             {
@@ -133,19 +140,18 @@ City *search_City(const char *city_name, bool searching_previous, City *list) //
     return return_value;
 }
 
-int distance_cities(City *city_1, City *city_2)
+int distance_cities(float lat1, float long1, float lat2, float long2) // renvoie la distance qui sépare les deux emplacements dont les coordonnées sont fournies
 {
-    float r = 6371;
-    float phi_1 = city_1->latitude * PI / 180;
-    float phi_2 = city_2->latitude * PI / 180;
+    float phi_1 = lat1 * PI / 180;
+    float phi_2 = lat2 * PI / 180;
     float delta_phi = phi_1 - phi_2;
-    float lambda_1 = city_1->longitude * PI / 180;
-    float lambda_2 = city_2->longitude * PI / 180;
+    float lambda_1 = long1 * PI / 180;
+    float lambda_2 = long2 * PI / 180;
     float delta_lambda = lambda_1 - lambda_2;
-    return 2*r * asinf(sqrtf(powf(sinf(delta_phi / 2), 2) + cosf(phi_1) * cosf(phi_2) * powf(sinf(delta_lambda / 2), 2)));
+    return 2*R_TERRE * asinf(sqrtf(powf(sinf(delta_phi / 2), 2) + cosf(phi_1) * cosf(phi_2) * powf(sinf(delta_lambda / 2), 2)));
 }
 
-void save_csv(const char *file_path, City *list) // writes data from a list onto a csv file and deletes data of the list
+void save_csv(const char *file_path, City *list) // écrit les données d'une liste de villes dans un fichier csv et supprime les données de la liste
 {
     FILE *csv = NULL;
     City *current = list->next;
@@ -156,7 +162,7 @@ void save_csv(const char *file_path, City *list) // writes data from a list onto
         free_Cities(list);
         exit(EXIT_FAILURE);
     }
-    fprintf(csv, "CODE,VILLE,LATITUDE,LONGITUDE\n"); // writing the title line
+    fprintf(csv, "CODE,VILLE,LATITUDE,LONGITUDE\n"); // écriture de la ligne des titres
     while (current != NULL)
     {
         fprintf(csv, "%s,%s,%f,%f\n", current->code, current->name, current->latitude, current->longitude);
@@ -166,7 +172,7 @@ void save_csv(const char *file_path, City *list) // writes data from a list onto
     free_Cities(list);
 }
 
-void user_add_city(City *list) // requests the user to add the city he wants to the list
+void user_add_city(City *list) // demande à l'utilisateur la ville qu'il veut ajouter à la liste et l'ajoute
 {
     City tmp;
     printf("Veuillez renseigner le nom de la ville à ajouter : ");
@@ -179,23 +185,23 @@ void user_add_city(City *list) // requests the user to add the city he wants to 
     printf("La Ville %s a bien été ajoutée.\n", tmp.name);
 }
 
-void user_del_city(City *list) // requests the user to delete the city he wants of the list
+void user_del_city(City *list) // demande à l'utilisateur la ville qu'il veut supprimer de la liste et la supprime
 {
-    char input[85];
+    char input[MAX_LEN_NAME];
     City *city_to_del = NULL;
     printf("Saisissez le Nom de la ville à supprimer : ");
     scanf(" %[^\n]", input);
     city_to_del = search_City(input, false, list);
-    if (city_to_del != NULL) // if the city has been found
+    if (city_to_del != NULL) // si la ville a été trouvée
     {
         free_City(city_to_del, list);
         printf("La Ville %s a bien été supprimée.\n", input);
     }
 }
 
-void user_chg_city(City *list) // requests the user to change the city he wants in the list
+void user_chg_city(City *list) // demande à l'utilisateur la ville qu'il veut modifier dans la liste et la modifie
 {
-    char input[85];
+    char input[MAX_LEN_NAME];
     City *previous_city = NULL;
     City *end = NULL;
     City *old = NULL;
@@ -203,12 +209,12 @@ void user_chg_city(City *list) // requests the user to change the city he wants 
     printf("Saisissez le Nom de la ville à modifier : ");
     scanf(" %[^\n]", input);
     previous_city = search_City(input, true, list);
-    if (previous_city != NULL) // if the previous city has been found
+    if (previous_city != NULL) // si la ville précédante a été trouvée
     {
         old = previous_city->next;
-        end = old->next; // saving the end of list
+        end = old->next; // sauvegarde de la fin de la liste
         printf("Anciennes données : %s / %s / %f / %f\n", old->code, old->name, old->latitude, old->longitude);
-        free(old); // deleting the city
+        free(old); // suppression de la ville
         previous_city->next = NULL;
         printf("Veuillez renseigner le nouveau code de %s : ", input);
         scanf(" %[^\n]", tmp.code);
@@ -216,75 +222,94 @@ void user_chg_city(City *list) // requests the user to change the city he wants 
         scanf(" %[^\n]", tmp.name);
         printf("Veuillez renseigner la latitude puis la longitude en degrés (ex : 52.56898 -86.565325) : ");
         scanf("%f %f", &tmp.latitude, &tmp.longitude);
-        create_city(tmp.code, tmp.name, tmp.latitude, tmp.longitude, end, list); // recreating the city at the same place in the list and reattaching the end of list
+        create_city(tmp.code, tmp.name, tmp.latitude, tmp.longitude, end, list); // recréation de la ville au même endroit dans la liste et réattachement de la fin de la liste
         printf("La Ville %s a bien été modifiée.\n", input);
     }
 }
 
-void user_coord_city(City *list) // requests the user to show him the coords of the city he wants
+void user_coord_city(City *list) // demande à l'utilisateur la ville dont il veut obtenir les coordonnées et affiche ces coordonnées
 {
-    char input[85];
+    char input[MAX_LEN_NAME];
     City *city_to_aff = NULL;
     printf("Saisissez le Nom de la ville dont vous souhaitez les coordonnées : ");
     scanf(" %[^\n]", input);
     city_to_aff = search_City(input, false, list);
-    if (city_to_aff != NULL) // if the city has been found
+    if (city_to_aff != NULL) // si la ville a été trouvée
     {
         printf("%s se situe aux coordonnées (lat / long) : %f / %f .\n", input, city_to_aff->latitude, city_to_aff->longitude);
     }
 }
 
-void user_distance_min_cities(City *list)
+void user_distance_min_cities(City *list) // demande à l'utilisateur les villes dont il veut connaitre la distance qui les sépare
 {
-    char input1[85], input2[85];
+    char city_name1[MAX_LEN_NAME], city_name2[MAX_LEN_NAME];
     City *city1 = NULL, *city2 = NULL;
     int dist = 0;
-    printf("Saisissez le Nom de la première ville : \n");
-    scanf(" %[^\n]", input1);
-    city1 = search_City(input1, false, list);
+    printf("Saisissez le Nom de la première ville : ");
+    scanf(" %[^\n]", city_name1);
+    city1 = search_City(city_name1, false, list);
     if (city1 == NULL)
     {
         return;
     }
-    printf("Saisissez le Nom de la deuxième ville : \n");
-    scanf(" %[^\n]", input2);
-    city2 = search_City(input2, false, list);
+    printf("Saisissez le Nom de la deuxième ville : ");
+    scanf(" %[^\n]", city_name2);
+    city2 = search_City(city_name2, false, list);
     if (city2 == NULL)
     {
         return;
     }
-    dist = distance_cities(city1, city2);
-    printf("La distance entre %s et %s est %d km.\n", input1, input2, dist);
+    dist = distance_cities(city1->latitude, city1->longitude, city2->latitude, city2->longitude);
+    printf("La distance entre %s et %s est %d km.\n", city_name1, city_name2, dist);
 }
 
-void print_cities(City *list) // prints all cities from the list
+void user_how_far_from_city(City *list) // demande à l'utilisateur ses coordonnées GPS et la ville dont il veu connaitre son éloignement
+{
+    char city_name[MAX_LEN_NAME];
+    float user_lat, user_long;
+    City *city = NULL;
+    int dist = 0;
+    printf("Saisissez le Nom de la ville : ");
+    scanf(" %[^\n]", city_name);
+    city = search_City(city_name, false, list);
+    if (city == NULL)
+    {
+        return;
+    }
+    printf("Veuillez saisir vos coordonnées GPS (latitude puis longitude en degrés) : ");
+    scanf("%f %f", &user_lat, &user_long);
+    dist = distance_cities(city->latitude, city->longitude, user_lat, user_long);
+    printf("La distance qui vous sépare de %s est %d km.\n", city_name, dist);
+}
+
+void print_cities(City *list) // affiche toutes les villes de la liste
 {
     City *current = list->next;
-    char tmp[15];
+    char tmp[MAX_LEN_CODE];
     printf("+-----------------+--------------------------------+-------------+-------------+\n");
     printf("| CODE            | VILLE                          | LATITUDE    | LONGITUDE   |\n");
     printf("+-----------------+--------------------------------+-------------+-------------+\n");
     while (current != NULL)
     {
         printf("| %s", current->code);
-        for (int i = 0; i < 16 - strlen(current->code); i++)
+        for (int i = 0; i < MAX_LEN_CODE + 1 - strlen(current->code); i++)
         {
             printf(" ");
         }
         printf("| %s", current->name);
-        for (int i = 0; i < 31 - strlen(current->name); i++)
+        for (int i = 0; i < MAX_LEN_NAME + 1 - strlen(current->name); i++)
         {
             printf(" ");
         }
         sprintf(tmp, "%f", current->latitude);
         printf("| %f", current->latitude);
-        for (int i = 0; i < 12 - strlen(tmp); i++)
+        for (int i = 0; i < MAX_LEN_LAT_LONG + 1 - strlen(tmp); i++)
         {
             printf(" ");
         }
         sprintf(tmp, "%f", current->longitude);
         printf("| %f", current->longitude);
-        for (int i = 0; i < 12 - strlen(tmp); i++)
+        for (int i = 0; i < MAX_LEN_LAT_LONG + 1 - strlen(tmp); i++)
         {
             printf(" ");
         }
@@ -294,7 +319,7 @@ void print_cities(City *list) // prints all cities from the list
     printf("+-----------------+--------------------------------+-------------+-------------+\n");
 }
 
-void show_help(void) // prints the list of possible commands
+void show_help(void) // affiche la liste des commandes possibles
 {
     printf("--------------------------------------------------------------\n");
     printf("Commandes disponibles :\n");
@@ -305,12 +330,13 @@ void show_help(void) // prints the list of possible commands
     printf("m - permet de modifier une ville\n");
     printf("c - permet d'afficher les coordonnées GPS d'une ville\n");
     printf("d - permet d'afficher la distance entre deux villes\n");
+    printf("f - permet d'afficher la distance qui vous sépare d'une ville\n");
     printf("e - enregistre les modifications et quitte le programme\n");
     printf("q - quitte le programme en abandonnant les modifications\n");
     printf("--------------------------------------------------------------\n");
 }
 
-void execute_command(char cmd, bool *exit, const char *file_path, City *list_cities) // executes the given command
+void execute_command(char cmd, bool *exit, const char *file_path, City *list_cities) // exécute la commande donnée
 {
     switch (cmd)
     {
@@ -349,15 +375,22 @@ void execute_command(char cmd, bool *exit, const char *file_path, City *list_cit
             user_distance_min_cities(list_cities);
             break;
         }
+        case 'f':
+        {
+            user_how_far_from_city(list_cities);
+            break;
+        }
         case 'e':
         {
             save_csv(file_path, list_cities);
+            printf("Les modifications ont bien été enregistrées dans %s.\n", file_path);
             *exit = true;
             break;
         }
         case 'q':
         {
             free_Cities(list_cities);
+            printf("Les modifications ont été abandonnées.\n");
             *exit = true;
             break;
         }
@@ -369,35 +402,35 @@ void execute_command(char cmd, bool *exit, const char *file_path, City *list_cit
     }
 }
 
-/* define Variables */
+/* définition des Variables */
 
 City list_cities;
-char csv_file_name[50];
+char csv_file_name[MAX_LEN_CSV_FILENAME];
 char command;
 bool quit;
 
-/* main Function */
+/* Fonction principale */
 
 int main(void)
 {
-    // initializing variables
+    // initialisation des variables
 
     list_cities.next = NULL;
     quit = false;
 
-    // obtain csv file path
+    // obtention du chemin du fichier csv
 
     printf("Saisissez le nom du fichier csv : ");
     strcpy(csv_file_name, "cities.csv");
     printf("%s\n", csv_file_name);
     // scanf("%s", csv_file_name);
 
-    // loading data & showing commands
+    // chargement des données & affichage des commandes
 
     load_from_csv(csv_file_name, &list_cities);
     show_help();
     
-    // main loop
+    // boucle principale
 
     while (!quit)
     {
@@ -406,7 +439,7 @@ int main(void)
         execute_command(command, &quit, csv_file_name, &list_cities);
     }
 
-    // quitting program
+    // fin de l'exécution du programme
 
     return EXIT_SUCCESS;
 }
